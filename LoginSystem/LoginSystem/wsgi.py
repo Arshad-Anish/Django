b2c2f9becbf11c15efb3dbc9f01cdc982297613f
anish <amanishmadhu@gmail.com>
2022-07-12 02:02:47 +0530

    wip
    wip

--- LoginSystem/LoginSystem/wsgi.py
+++ LoginSystem/LoginSystem/wsgi.py
@@ -10,7 +10,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LoginSystem.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LoginSystem.settings")
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("login/", include("home.urls")),
+]
 
 application = get_wsgi_application()

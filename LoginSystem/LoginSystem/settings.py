3660a10d5d31ea2d13ae700e55ee0013c16bac56
Arshad-Anish <amanishmadhu@gmail.com>
2022-07-19 19:18:51 +0530

    wip

--- LoginSystem/LoginSystem/settings.py
+++ LoginSystem/LoginSystem/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-LOGIN_REDIRECT_URL = 'home/'
+LOGIN_REDIRECT_URL = "home/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -124,6 +124,3 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-
